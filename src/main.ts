03e982b0e1424f754a6c2aaa39fbb352c273e69f
go-tiger <dev.gotiger@gmail.com>
2025-01-22 02:39:26 +0900

    feat: swagger 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { setupSwagger } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configservice = app.get(ConfigService);
+
+  setupSwagger(app);
+
   const PORT = configservice.get<number>('PORT');
 
   await app.listen(PORT ?? 3000);
