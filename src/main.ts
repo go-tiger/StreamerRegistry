1aecee0676390343219116bb179bf1f4b8a3841c
go-tiger <dev.gotiger@gmail.com>
2025-01-22 00:58:55 +0900

    feat: 환경 변수 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configservice = app.get(ConfigService);
+  const PORT = configservice.get<number>('PORT');
+
+  await app.listen(PORT ?? 3000);
+  console.log('🚀  PORT:', PORT);
 }
 bootstrap();
