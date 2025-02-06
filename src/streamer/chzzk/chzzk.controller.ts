335db6a3427c6c6ca102099cb4a30479616c20da
go-tiger <dev.gotiger@gmail.com>
2025-02-06 09:12:45 +0900

    fix: 오타 수정

--- src/streamer/chzzk/chzzk.controller.ts
+++ src/streamer/chzzk/chzzk.controller.ts
@@ -17,7 +17,7 @@ export class ChzzkController {
   @Post()
   @ApiOperation({ description: '치지직 스트리머 등록' })
   @ApiNotFoundResponse({ description: '먼저 플레이어로 등록해주세요.' })
-  @ApiConflictResponse({ description: '이미 등록된 치지직 플레이어입니다.' })
+  @ApiConflictResponse({ description: '이미 등록된 치지직 스트리머입니다.' })
   @ApiBadRequestResponse({ description: '해당 플랫폼과 스트리머가 이미 등록되었습니다.' })
   @ApiInternalServerErrorResponse({ description: '플레이어 등록 중 오류가 발생했습니다.' })
   async registerPlayer(@Body() dto: CreateChzzkDto) {
